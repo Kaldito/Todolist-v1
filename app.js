bb84fd26eb36e33dcbe442c22a774f9e74a6879e
Kaldo <dantekaled100@gmail.com>
2022-12-15 23:48:29 -0600

    Todo List v2.0

--- app.js
+++ app.js
@@ -1,50 +1,116 @@
+// -------------- PACKAGES -------------- //
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const _ = require('lodash');
 
-let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-let items = ["Jugar LoL", "Comer", "Dormir"];
-let workItems = [];
+// -------------- ASYNC FUNCTIONS -------------- //
+async function insertEntry(mod, arr){
+    try{
+        await mod.insertMany(arr)
+        console.log("Succesfully saved on DB");
+    } catch(err){
+        console.log(err);
+    }
+}
 
+async function showCollection(mod, filter){
+    try{
+        const documents = await mod.find(filter);
+        return documents;
+    } catch(err){
+        console.log(err);
+    }
+}
+
+// -------------- APP CONFIG -------------- //
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public")); // Uploads the public folder
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-    let day = date.getDate();
+// -------------- DATABASE -------------- //
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://0.0.0.0:27017/todolistDB");
 
-    res.render("list", {listTitle: day, itemList: items});
+// -------------- SCHEMAS -------------- //
+const itemSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+    },
+    listName: String
 });
 
-app.post("/", function(req, res){
+// -------------- MODELS -------------- //
+const Item = mongoose.model("Item", itemSchema);
+
+// -------------- SERVER -------------- //
+// -- GET METHOD -- //
+// - Home
+app.get("/", async function(req, res){
+    const items = await showCollection(Item, {listName: "/"});
+
+    res.render("list", {listTitle: "My daily list", itemList: items, r: "/"});
+});
+
+// - Custom List
+app.get("/:listPath", async function(req, res){
+    const list = _.startCase(req.params.listPath);
+    const route = "/" + list;
+    const items = await showCollection(Item, {listName: route});
+
+    res.render("list", {listTitle: list, itemList: items, r: route});
+}) 
+
+// -- POST METHOD -- //
+// - Home
+app.post("/", async function(req, res){
     const todo = req.body.todo;
 
-    if(req.body.list == "Work List") {
-        workItems.push(todo);
-        res.redirect("/work");
-    } else{
-        items.push(todo);
-        res.redirect("/")
+    const newItem = new Item({
+        name: todo,
+        listName: "/"
+    });
+
+    await insertEntry(Item, [newItem]);
+
+    res.redirect("/")
+});
+
+// - Custom List
+app.post("/:listPath", async function(req, res){
+    const todo = req.body.todo;
+    const list = _.startCase(req.params.listPath);
+    const route = "/" + list;
+
+    const newItem = new Item({
+        name: todo,
+        listName: route
+    });
+
+    await insertEntry(Item, [newItem]);
+
+    res.redirect(route)
+})
+
+// - DELETE ITEM
+app.post("/db/delete", async function(req, res){
+    const itemId = req.body.checkbox;
+
+    try{
+        deletedItem = await Item.findByIdAndDelete(itemId);
+    } catch(err){
+        console.log(err);
     }
+
+    route = deletedItem.listName;
+
+    res.redirect(route);
 })
 
-app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work List", itemList: workItems})
-})
-
-app.post("/work", function (req, res) {
-    const item = req.body.todo;
-    workItems.push(item);
-    res.redirect("/work");
-})
-
+// -- SERVER LISTEN -- //
 app.listen(3000, function(){
     console.log("The server is running on port 3000");
-})
-
-
-
-
-
-
+});
